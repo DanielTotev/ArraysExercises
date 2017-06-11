acdc75d414f46622eca665318867e875234e1118
DanielTotev <dtotev1@gmail.com>
2017-06-11 16:29:50 +0300

    Added some more problems

---
+++ RoundNumbers/RoundNumbers.cs
@@ -0,0 +1,18 @@
+﻿namespace RoundNumbers
+{
+    using System;
+    using System.Linq;
+
+    public class RoundNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(decimal.Parse).ToArray();
+            var result = numbers.Select(x => Math.Round(x , MidpointRounding.AwayFromZero)).ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {result[i]}");
+            }
+        }
+    }
+}
---
+++ SieveOfEratosthenes/FindAllPrimes.cs
@@ -0,0 +1,37 @@
+﻿namespace SieveOfEratosthenes
+{
+    using System;
+  
+    using System.Collections.Generic;
+    public class FindAllPrimes
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool[] arr = new bool[n + 1];
+            for (int i = 2; i < n; i++)
+            {
+                arr[i] = true;
+            }
+            List<int> result = new List<int>();
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (arr[i] == true)
+                {
+                    for (int j = 2; j * i < n; j+= i)
+                    {
+                        arr[(j * i)] = false;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == true)
+                {
+                    result.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
